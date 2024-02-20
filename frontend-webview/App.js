b9cb8e54f898b48a7f27a750bb3d52771ed4c8de
Adam Martiska <adam.martiska@kistler.com>
2024-02-20 14:10:08 +0100

    Add dark mode into native app

--- frontend-webview/App.js
+++ frontend-webview/App.js
@@ -1,17 +1,44 @@
-import { SafeAreaView, StyleSheet } from "react-native";
+import React from "react";
+import { Platform, SafeAreaView, StyleSheet } from "react-native";
 import { WebView } from "react-native-webview";
 
 export default function App() {
   const webViewRef = React.useRef(null);
+  const [mode, setMode] = React.useState();
 
   const provideDeviceInformation = () => {
     if (webViewRef?.current) {
-      webViewRef?.current?.postMessage("Cauko mnaukoo");
+      webViewRef?.current?.postMessage(
+        Platform.OS === "ios" ? "MobileIos" : "MobileAndroid"
+      );
+    }
+  };
+
+  const debugging = `
+  const consoleLog = (type, log) => window.ReactNativeWebView.postMessage(JSON.stringify({'type': 'Console', 'data': {'type': type, 'log': log}}));
+  console = {
+      log: (log) => consoleLog('log', log),
+      debug: (log) => consoleLog('debug', log),
+      info: (log) => consoleLog('info', log),
+      warn: (log) => consoleLog('warn', log),
+      error: (log) => consoleLog('error', log),
+    };
+`;
+
+  const onMessage = (payload) => {
+    // payload.nativeEvent.data contains either 'Dark' or 'Light'
+    if (typeof payload.nativeEvent.data === "string") {
+      setMode(payload.nativeEvent.data);
     }
   };
 
   return (
-    <SafeAreaView style={styles.container}>
+    <SafeAreaView
+      style={[
+        styles.container,
+        mode === "Dark" ? { backgroundColor: "#1E1E1E" } : {},
+      ]}
+    >
       <WebView
         ref={webViewRef}
         allowsBackForwardNavigationGestures
@@ -19,7 +46,20 @@ export default function App() {
         source={{
           uri: "https://app.offli.eu",
         }}
+        mediaCapturePermissionGrantType="grant"
+        startInLoadingState
+        javaScriptEnabled
+        domStorageEnabled
+        cacheEnabled
+        thirdPartyCookiesEnabled
+        allowsProtectedMedia
+        allowUniversalAccessFromFileURLs
+        allowsInlineMediaPlayback
+        mediaPlaybackRequiresUserAction={false}
         onLoadEnd={provideDeviceInformation}
+        onMessage={onMessage}
+        webviewDebuggingEnabled={true}
+        injectedJavaScript={debugging}
       />
     </SafeAreaView>
   );
@@ -29,9 +69,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     overflow: "hidden",
-    // backgroundColor: "#fff",
-    // alignItems: "center",
-    // justifyContent: "center",
   },
   webview: {
     flex: 1,
